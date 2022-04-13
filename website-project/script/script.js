19576110f409b2b22ce82a16f9de4af0dae37ffc
Karolina <karolina.kotwica@powercoders.org>
2022-04-13 22:30:39 +0200

    Create function that changes the color of words on the page

--- website-project/script/script.js
+++ website-project/script/script.js
@@ -7,3 +7,51 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+function changeColor() {
+
+    var replacement = "<span class='yellow'>repla</span>";
+
+    var words = ["ls", "cp", "cd", "cd ~", "-a", "-d", "cs ..", "mkdir", "rmdir", "rm", "touch", "pwd"];
+
+    for (i=0;i<words.length;i++){
+
+        var str = document.getElementById("replace").innerHTML;
+
+        var replacethis = words[i];
+
+        var re = new RegExp(replacethis,"g");
+
+        var txt = str.replace(re, replacement.replace("repla",replacethis) );
+
+        document.getElementById("replace").innerHTML = txt;
+
+    }
+
+}
+
+function makeCursive() {
+
+    var replacement = "<span class='cursive'>repla</span>";
+
+    var words = ["name", "origin", "destination", "word"];
+
+    for (i=0;i<words.length;i++){
+
+        var str = document.getElementById("replace").innerHTML;
+
+        var replacethis = words[i];
+
+        var re = new RegExp(replacethis,"g");
+
+        var txt = str.replace(re, replacement.replace("repla",replacethis) );
+
+        document.getElementById("replace").innerHTML = txt;
+
+    }
+
+}
+
+makeCursive();
+changeColor();
