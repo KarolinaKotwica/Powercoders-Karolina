239dbc589e5c138c897c96f7e2afd97d208bbead
Karolina <karolina.kotwica@powercoders.org>
2022-04-21 11:53:52 +0200

    Create multi.js file and write Multiplication Tables solution

---
+++ week03/multiplication-tables/multi.js
@@ -0,0 +1,14 @@
+// Multiplication Tables
+
+let numb = Number(window.prompt("Write a number"))
+let multiply = numb * 9;
+
+console.log(`${numb} * 9 = ${multiply}`);
+
+
+// Multiplication Tables 2.0
+
+for ( let i = 0; i <= 10; i++ ) {
+    let multiply = i * 9;
+    console.log( `${i} * 9 = ${multiply}` )
+}
