6ac54ac2a5bf283e400ca4143ded68392f6c3990
Karolina <karolina.kotwica@powercoders.org>
2022-04-28 09:30:45 +0200

    Move two exercies to another folder

--- week04/topYourChoices/topYourChoices.js
+++ week04/topYourChoices/topYourChoices.js
@@ -1,15 +1,15 @@
 //// Array //////
 
-// const choices = ['English', "German", "Spanish"];
+const choices = ['English', "German", "Spanish"];
 
-// for (let i = 0; i < choices.length; i++) {
-//     console.log(`My #${i+1} choice is ${choices[i]}.`);
-// }
+for (let i = 0; i < choices.length; i++) {
+    console.log(`My #${i+1} choice is ${choices[i]}.`);
+}
 
 ////// change into Objects ////
 
-// const choicesObject = {
-//     language1: "English",
-//     language2: 'German',
-//     language3: "Spanish"
-// }
+const choicesObject = {
+    language1: "English",
+    language2: 'German',
+    language3: "Spanish"
+}
