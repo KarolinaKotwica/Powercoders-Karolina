7a10cc5db44b2ea4c3286dd1ea91777ecf20e0ff
Karolina <karolina.kotwica@powercoders.org>
2022-04-29 09:09:36 +0200

    Add Exercises

---
+++ week05/arrays/arrays.js
@@ -0,0 +1,23 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1.
+let oranges = array[2];
+console.log(oranges);
+
+// 2.
+array.shift();
+console.log(array);
+
+// 3.
+array.sort();
+
+//4. 
+array.push("Kiwi");
+
+// 5.
+array.shift();
+
+// 6.
+array.reverse();
+
+console.log(array);
---
+++ week05/recipeCard/recipe.js
@@ -0,0 +1,9 @@
+const recipes = {
+    title: "soup",
+    servings : 2,
+    ingredients: ['cinnamon', 'cumin', 'cocoa']
+  }
+  
+console.log(recipes.title);
+console.log(`Serves: ${recipes.servings}`);
+console.log(`Ingredients: ${recipes.ingredients}`);
