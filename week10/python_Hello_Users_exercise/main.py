631a4e2fcdf142a1b87a6ed8094ccdb478a38251
Karolina <karolina.kotwica@powercoders.org>
2022-05-31 14:48:41 +0200

    Add Hello Users exercise

---
+++ week10/python_Hello_Users_exercise/main.py
@@ -0,0 +1,25 @@
+users = [
+	{
+		"first_name": "Brain",
+		"last_name": "Mohr",
+		"age": 54
+	},
+	{
+		"first_name": "Bella",
+		"last_name": "VonRueden",
+		"age": 17
+	},
+	{
+		"first_name": "Franz",
+		"last_name": "Raynor",
+		"age": 28
+	},
+    {
+		"first_name": "Celestino",
+		"last_name": "Bailey",
+		"age": 61
+    }
+]  
+
+for user in users:
+  print(f"My name is {user['first_name']} and I am {user['age']} years old")
